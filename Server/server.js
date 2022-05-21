6118b582329bc311fddb9edec7b4df30bc627366
Juan Villegas <jvillegas422@gmail.com>
2022-05-21 11:53:55 -0500

    Updated install for server

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,16 @@
 console.log('In the server!');
 
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.json());
 
 // Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('server/public'));
+app.use(express.static());
 
-
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log ('Server is running on port', PORT)
-  })
+  });
