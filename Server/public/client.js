8fb0a42b718b1fad357a4cb8a9f2a53a8fee7f6e
Juan Villegas <jvillegas422@gmail.com>
2022-05-21 12:27:30 -0500

    Added get & post in client.js & server.js

--- Server/public/client.js
+++ Server/public/client.js
@@ -1,2 +1,28 @@
 console.log('In JS');
 
+$(document).ready(onReady);
+
+function onReady() {
+    console.log("jquery is loaded!");
+    $('#calc-answer').on('click', calcAnswer);
+}
+
+function calcAnswer(evt) {
+    evt.preventDefault;
+    let calculate = {
+        firstNum: $('#first-input').val(),
+        secondNum: $('#second-input').val()
+    };
+    console.log('Calculate first & second numbers');
+
+$.ajax({
+url: '/guess',
+method: 'POST',
+data: calculate
+}).then((response) => {
+    console.log(
+    'POST request works',
+    response
+    );
+  });
+}
--- Server/server.js
+++ Server/server.js
@@ -10,6 +10,20 @@ const express = require('express');
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static());
 
+let calcAnswer = [];
+
+app.get('/calculate', (req, res) => {
+    console.log('Calculate is working.');
+    res.send(calcAnswer);
+})
+
+app.post('/calculate', (req, res) => {
+    console.log('Made it to post /calculate.');
+    calcAnswer.push(req.body)
+    console.log(calcAnswer)
+      res.sendStatus(201);
+})
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log ('Server is running on port', PORT)
