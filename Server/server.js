af6b976b1db48ee331f65890ae01d11196e000ad
Juan Villegas <jvillegas422@gmail.com>
2022-05-21 11:24:03 -0500

    Updating server

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,16 @@
 console.log('In the server!');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// This must be added before GET & POST routes.
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
+// Serve up static files (HTML, CSS, Client JS)
+app.use(express.static('server/public'));
+
+
+app.listen(PORT, () => {
+    console.log ('Server is running on port', PORT)
+  })
